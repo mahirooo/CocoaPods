571626458344fd38383877b282c60ad71a5a3956
mahirooo <mahiro.nagasaki@gmail.com>
2022-08-31 08:03:30 +0900

    all done

---
+++ CocoaPodsApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  CocoaPodsApp
+//
+//  Created by 長崎茉優 on 2022/08/31.
+//
+
+import UIKit
+import PKHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        HUD.flash(.success, delay: 2.0)
+    }
+
+}
+
